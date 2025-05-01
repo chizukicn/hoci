0de029203a797e227647c04f026d728cc28e2e8d
chizukicn <chizukicn@outlook.com>
2025-05-02 00:23:55 +0800

    docs: add switch docs

---
+++ docs/examples/switch/basic.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from "vue";
+
+const checked = ref(false);
+</script>
+
+<template>
+  <div class="flex items-center gap-4">
+    <!-- 基础用法 -->
+    <hi-switch
+      v-model="checked"
+      active-class="bg-blue-500 text-white shadow-md"
+      unactive-class="bg-gray-200 text-gray-700 border border-gray-300"
+      class="min-w-16 text-center px-4 py-1 rounded-lg duration-300 select-none cursor-pointer"
+    >
+      <template #default="{ active }">
+        {{ active ? "开" : "关" }}
+      </template>
+    </hi-switch>
+  </div>
+</template>
+
+<style>
+</style>
---
+++ docs/examples/switch/disabled.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from "vue";
+
+const checked = ref(false);
+</script>
+
+<template>
+  <hi-switch v-model="checked" :disabled="true" disabled-class="text-#aaa bg-#f5f5f5 px-4 py-1 min-w-16 rounded-lg cursor-not-allowed">
+    <template #default="{ active, isDisabled }">
+      {{ isDisabled ? "禁用" : active ? "开" : "关" }}
+    </template>
+  </hi-switch>
+</template>

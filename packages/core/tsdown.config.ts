cf569c45aa645f74e75b63242d626cb000f9693c
chizukicn <chizukicn@outlook.com>
2025-10-10 10:03:45 +0800

    chore: update

--- packages/core/tsdown.config.ts
+++ packages/core/tsdown.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "tsdown";
 export default defineConfig({
   dts: true,
   format: ["cjs", "esm"],
-  entry: "./index.ts",
+  entry: "index.ts",
   exports: true
 });
--- packages/hoci/tsdown.config.ts
+++ packages/hoci/tsdown.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "tsdown";
 export default defineConfig({
   dts: true,
   format: ["cjs", "esm"],
-  entry: ["./index.ts", "./resolver.ts"],
+  entry: ["index.ts", "resolver.ts"],
   exports: true
 });

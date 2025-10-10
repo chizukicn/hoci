f607569e53fb7e5112403c903beaa0a9d4ce0876
chizukicn <chizukicn@outlook.com>
2025-10-10 10:00:00 +0800

    chore: up deps, and use tsdown instead of unbuild

--- packages/hoci/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { install } from "@hoci/components";
-
-export * from "@hoci/components";
-export * from "@hoci/core";
-export default { install };
---
+++ packages/hoci/tsdown.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "tsdown";
+
+export default defineConfig({
+  dts: true,
+  format: ["cjs", "esm"],
+  entry: ["./index.ts", "./resolver.ts"],
+  exports: true
+});
